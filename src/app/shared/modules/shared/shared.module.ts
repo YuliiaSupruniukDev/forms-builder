fb31d72d80a80bc75b9eda2a606e06147c00ccae
yuliiasupruniuk <juliasupruniuk23@gmail.com>
2022-06-22 12:30:30 +0300

    updated modules, added elements

--- src/app/shared/modules/shared/shared.module.ts
+++ src/app/shared/modules/shared/shared.module.ts
@@ -1,12 +1,16 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
-
+import { InputComponent } from '../../components/input/input.component';
+import { MaterialUIModule } from '../material-ui/material-ui.module';
+import { TextareaComponent } from '../../components/textarea/textarea.component';
+import { SelectComponent } from '../../components/select/select.component';
+import { CheckboxComponent } from '../../components/checkbox/checkbox.component';
+import { ButtonComponent } from '../../components/button/button.component';
 
 @NgModule({
-  declarations: [],
-  imports: [
-    CommonModule
-  ]
+  declarations: [InputComponent, TextareaComponent,  CheckboxComponent, SelectComponent, ButtonComponent],
+  imports: [CommonModule, MaterialUIModule],
+  exports: [InputComponent, TextareaComponent, CheckboxComponent, SelectComponent, ButtonComponent],
 })
-export class SharedModule { }
+export class SharedModule {}
