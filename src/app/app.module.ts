d5440ef3bffbd61d535bd6fe083a39ecfc66b7a3
yuliiasupruniuk <juliasupruniuk23@gmail.com>
2022-06-22 12:28:50 +0300

    updated main module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,22 +8,21 @@ import { AuthComponent } from './core/pages/auth/auth.component';
 import { MainComponent } from './core/pages/main/main.component';
 import { HeaderComponent } from './core/components/header/header.component';
 
-import {MatIconModule} from '@angular/material/icon';
+import { MatIconModule } from '@angular/material/icon';
+import { FormsModule } from '@angular/forms';
+import { SharedModule } from './shared/modules/shared/shared.module';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    AuthComponent,
-    MainComponent,
-    HeaderComponent,
-  ],
+  declarations: [AppComponent, AuthComponent, MainComponent, HeaderComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
+    FormsModule,
     MatIconModule,
+    SharedModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
