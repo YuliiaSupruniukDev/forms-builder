20a32c8d42f0fb9255da7a9f0777887d76800fa0
yuliiasupruniuk <juliasupruniuk23@gmail.com>
2022-06-23 14:31:11 +0300

    updated main module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AuthComponent } from './pages/auth/auth.component';
-import { MainComponent } from './pages/main/main.component';
 
 const routes: Routes = [
-  { path: '', component: MainComponent },
+  {
+    path: '',
+    loadChildren: () =>
+      import('src/app/pages/main/main.module').then((m) => m.MainModule),
+  },
   { path: 'login', component: AuthComponent },
 ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,36 +5,27 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
-import { MatIconModule } from '@angular/material/icon';
 import { FormsModule } from '@angular/forms';
 import { SharedModule } from './shared/modules/shared.module';
 import { StylingModule } from './modules/styling/styling.module';
 import { CommonModule } from '@angular/common';
 import { AuthComponent } from './pages/auth/auth.component';
-import { MainComponent } from './pages/main/main.component';
 import { FormBuilderModule } from './modules/form-builder/form-builder.module';
 import { FormFieldsModule } from './modules/form-fields/form-fields.module';
 import { HeaderModule } from './modules/header/header.module';
 import { DragDropModule } from '@angular/cdk/drag-drop';
+import { MainComponent } from './pages/main/components/main/main.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    AuthComponent,
-    MainComponent
+    AuthComponent
   ],
   imports: [
     CommonModule,
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    FormsModule,
-    SharedModule,
-    StylingModule,
-    FormBuilderModule,
-    FormFieldsModule,
-    HeaderModule,
-    DragDropModule
   ],
   providers: [],
   bootstrap: [AppComponent],
