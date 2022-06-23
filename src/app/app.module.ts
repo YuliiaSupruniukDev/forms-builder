d54e9a8a5846859ab85eabe446670cc496600945
yuliiasupruniuk <juliasupruniuk23@gmail.com>
2022-06-23 16:51:30 +0300

    deleted unused imports

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,16 +5,8 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
-import { FormsModule } from '@angular/forms';
-import { SharedModule } from './shared/modules/shared.module';
-import { StylingModule } from './modules/styling/styling.module';
 import { CommonModule } from '@angular/common';
 import { AuthComponent } from './pages/auth/auth.component';
-import { FormBuilderModule } from './modules/form-builder/form-builder.module';
-import { FormFieldsModule } from './modules/form-fields/form-fields.module';
-import { HeaderModule } from './modules/header/header.module';
-import { DragDropModule } from '@angular/cdk/drag-drop';
-import { MainComponent } from './pages/main/components/main/main.component';
 
 @NgModule({
   declarations: [
