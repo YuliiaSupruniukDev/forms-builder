f3a8ce7286751a1028ba90d5abf5e30118fa6720
yuliiasupruniuk <juliasupruniuk23@gmail.com>
2022-06-27 12:21:03 +0300

    fixed deps

--- src/app/state/reducers/fields.reducer.ts
+++ src/app/state/reducers/fields.reducer.ts
@@ -1,17 +1,10 @@
 import { createReducer, on } from '@ngrx/store';
 import { IElement } from 'src/app/interfaces/element.interface';
 import { setFields } from '../actions/field.actions';
-import { v4 as uuid } from 'uuid';
 
 export const initialState: IElement[] = [];
 
 export const fieldStyleReducer = createReducer(
   initialState,
-  on(setFields, (state, { fields }) => {
-    fields.forEach((field) => {
-      field.key = uuid();
-    });
-
-    return fields;
-  })
+  on(setFields, (state, { fields }) => fields)
 );
