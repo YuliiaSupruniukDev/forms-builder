0aeb8fd0f4f42a5a2b1faa177e5f92451e9d5b18
yuliiasupruniuk <juliasupruniuk23@gmail.com>
2022-06-27 19:29:33 +0300

    added field deletetion

--- src/app/shared/services/drag-and-drop.service.ts
+++ src/app/shared/services/drag-and-drop.service.ts
@@ -1,18 +1,21 @@
 import { CdkDragDrop, moveItemInArray } from '@angular/cdk/drag-drop';
 
 import { EFields } from 'src/app/enums/fields.enum';
-import { IElementItemKey } from 'src/app/interfaces/element.interface';
+import { IElement } from 'src/app/interfaces/element.interface';
 import { Injectable } from '@angular/core';
+import { StyleService } from './style.service';
 import { v4 as uuid } from 'uuid';
 
 @Injectable({
   providedIn: 'root',
 })
 export class DragAndDropService {
+  constructor(private styleService: StyleService) {}
+
   public drop(
     event: CdkDragDrop<string[]>,
-    elementsArr: IElementItemKey[]
-  ): IElementItemKey[] {
+    elementsArr: IElement[]
+  ): IElement[] {
     let item: string = event.previousContainer.data[event.previousIndex];
     event.container.data.splice(event.currentIndex, 0, item);
 
@@ -22,11 +25,13 @@ export class DragAndDropService {
   setElementsArray(
     id: number,
     item: string,
-    elementsArr: IElementItemKey[]
-  ): IElementItemKey[] {
-    let newField: IElementItemKey = {
+    elementsArr: IElement[]
+  ): IElement[] {
+    const type = EFields[item as keyof typeof EFields];
+    const newField: IElement = {
       key: uuid(),
-      type: EFields[item as keyof typeof EFields],
+      type,
+      style: this.styleService.getInitialStyle(type),
     };
 
     elementsArr.splice(id, 0, newField);
--- src/app/shared/services/field-transfer.service.ts
+++ src/app/shared/services/field-transfer.service.ts
@@ -1,12 +1,17 @@
 import { IElement } from 'src/app/interfaces/element.interface';
 import { Injectable } from '@angular/core';
+import { Store } from '@ngrx/store';
 import { Subject } from 'rxjs';
+import { setFields } from 'src/app/state/actions/field.actions';
 
 @Injectable({
   providedIn: 'root',
 })
 export class FieldTransferService {
   private field = new Subject<IElement>();
+  private formFields: IElement[] = [];
+
+  constructor(private store: Store) {}
 
   set pickedField(value: IElement) {
     this.field.next(value);
@@ -14,5 +19,14 @@ export class FieldTransferService {
 
   get pickedField(): any {
     return this.field;
+  }
+
+  set fildsList(array: IElement[]) {
+    this.formFields = array;
+    this.store.dispatch(setFields({ fields: [...array] }));
+  }
+
+  deleteFormField(key: string) {
+    this.fildsList = this.formFields.filter((field) => field.key !== key);
   }
 }
