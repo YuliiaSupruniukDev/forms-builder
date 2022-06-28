f3b1c5eef7652b742391e80516e139f5fc63f15b
YuliiaSupruniukDev <107845563+YuliiaSupruniukDev@users.noreply.github.com>
2022-06-28 19:45:23 +0300

    Revert "Revert "Revert "develop"""

--- src/app/state/actions/enums/field.enum.ts
+++
@@ -1,5 +0,0 @@
-export enum FieldStyleActions {
-  SetFormFields = '[Fields Style] Set Form Fields',
-  SetFieldStyle = '[Fields Style] Set Field Style',
-  DeleteField = '[Fields Style] Delete Field',
-}
--- src/app/state/app.state.ts
+++
@@ -1,7 +0,0 @@
-import { IElement } from '../interfaces/element.interface';
-import { IFormStyleConfig } from '../interfaces/form.interface';
-
-export interface IAppState {
-  formStyle: IFormStyleConfig;
-  fieldsStyle: IElement[];
-}
